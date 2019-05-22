262eb17db0b0d72dee41541b8e9a03b2311a94be
Leszek Celary <209281@edu.p.lodz.pl>
2019-05-22 14:31:41 +0300

    InterfaceDiff implemented

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-public class Main {
+public class Main implements IArithmeticsDiff{
 
     public static void main(String[] args) {
         System.out.println("ZPI_2019_Dzienni_IO2_Stefan, developer - Dragoonov, DevOps - 209281-LeszekCelary, Dragoonov, tester - M4jk, developer - mateusznowak0");
+    }
+
+    @Override
+    public double Difference(double A, double B)
+    {
+        return A - B;
     }
 }
