561b791cb2e9e43fe2e3ba387be07f769af827e1
Leszek Celary <209281@edu.p.lodz.pl>
2019-05-22 14:49:34 +0300

    second comment added

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main implements IArithmeticsAdd, IArithmeticsDiv, IArithmeticsMult,
   
     @Override
     public double division(double A, double B) {
-        return A / B;
+        return A / B; //odejmowanie
     }
 
     @Override
