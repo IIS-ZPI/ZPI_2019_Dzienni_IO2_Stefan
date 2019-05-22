b748ea1ffa5170db2ca3b2674ce8d1d77af057ca
Dragoonov <jakub_lipowski@o2.pl>
2019-05-22 14:33:25 +0200

    Added interface and implementation of interface

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-public class Main {
+public class Main implements IArithmeticsAdd {
 
     public static void main(String[] args) {
         System.out.println("ZPI_2019_Dzienni_IO2_Stefan, developer - Dragoonov, DevOps - 209281-LeszekCelary, Dragoonov, tester - M4jk, developer - mateusznowak0");
+    }
+
+    @Override
+    public double Addition(double a, double b) {
+        return a+b;
     }
 }
