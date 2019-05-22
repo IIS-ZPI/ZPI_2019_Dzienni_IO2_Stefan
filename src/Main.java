8384c2528906391cca0526fc2740c47ed47135f2
Dragoonov <jakub_lipowski@o2.pl>
2019-05-22 14:50:34 +0200

    Added comment to multiplication

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main implements IArithmeticsAdd, IArithmeticsDiv, IArithmeticsMult,
     @Override
     public double multiplication(double A, double B) {
         return A * B;
+        // multiplication
     }
   
     @Override
